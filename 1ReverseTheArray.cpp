2850bd197ad50b4c75484fed17e9b2adca715413
sonukrs <80021435+sonukrs@users.noreply.github.com>
2021-05-29 19:07:09 +0530

    Add files via upload

---
+++ 1ReverseTheArray.cpp
@@ -0,0 +1,47 @@
+/*Reverse a String 
+Basic Accuracy: 56.22% Submissions: 30370 Points: 1
+You are given a string s. You need to reverse the string.
+
+Example 1:
+
+Input:
+s = Geeks
+Output: skeeG
+Example 2:
+
+Input:
+s = for
+Output: rof
+Your Task:
+
+You only need to complete the function reverseWord() that takes s as parameter and returns the reversed string.
+
+Expected Time Complexity: O(|S|).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= |s| <= 10000
+
+..................................................................................................................
+*/
+#include<iostream>
+using namespace std;
+string reverseWord(string str)
+{
+    int i=0;
+    int j=str.length()-1;
+    while(i<j)
+    {
+        swap(str[i],str[j]);
+        i++;
+        j--;
+    }
+    return str;
+}
+int main()
+{
+    string str;
+    cin>>str;
+    str=reverseWord(str);
+    cout<<str<<"\n";
+}
